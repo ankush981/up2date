d04140fc781bb074a76031ba3b6dd5c2b29baa14
Ankush <ankush.thakur53@gmail.com>
2016-02-07 11:41:12 +0530

    Added two exception classes

---
+++ errors.py
@@ -0,0 +1,13 @@
+"""Define the possible exceptions that the system can throw."""
+
+class LoginError (Exception):
+    def __init__(self, user): # to-do: Define user class
+        self.user = user
+
+    def get_error_message(self):
+        '''The default error messages. All derived classes must provide their own.'''
+        return "Unknown login error for email {}. Please retry.".format(self.user.email)
+
+class NoUserFoundError(LoginError):
+    def get_error_message(self):
+        return "No user found with email {}".format(self.user.email)
