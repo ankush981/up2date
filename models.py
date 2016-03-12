7610235716bb4cf6329fc9c552a5972cbf59a497
Ankush <ankush.thakur53@gmail.com>
2016-03-12 17:59:00 +0530

    Define many-to-many between user and website - yay! :D

--- models.py
+++ models.py
@@ -6,8 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Role(db.Model):
-    __tablename__ = 'roles'
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
@@ -15,13 +13,19 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
+# relationship table between users and websites
+user_website = db.Table('user_website', 
+                    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
+                    db.Column('website_id', db.Integer, db.ForeignKey('website.id'), nullable=False),
+                    db.PrimaryKeyConstraint('user_id', 'website_id')
+                )
+
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200), unique=True, index=True)
     password_hash = db.Column(db.String(200), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    websites = db.relationship('Website', secondary=user_website, backref='users')
     
     def __repr__(self):
         return '<User %r>' % self.email
@@ -38,16 +42,14 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
 
-# class Category(db.Model):
-#     __tablename__ = "categories"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String, unique=True)
-#     websites = db.relationship('websites', backref='category', lazy='dynamic')
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    websites = db.relationship('Website', backref='category', lazy='dynamic')
 
-# class Website(db.Model):
-#     __tablename__ = "websites"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-#     url = db.Column(db.String(1000), nullable=False)
-#     class_name = db.Column(db.String(50), nullable=False)
+class Website(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    url = db.Column(db.String(1000), nullable=False)
+    class_name = db.Column(db.String(50), nullable=False)
