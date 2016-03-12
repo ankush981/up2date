529d920a7c1e543c1961436353b744c7ed340ed4
Ankush <ankush.thakur53@gmail.com>
2016-03-12 21:01:33 +0530

    Fix UserManager : Now uses full power of SQLAlchemy! :D

--- managers.py
+++ managers.py
@@ -1,18 +1,8 @@
 """Manager classes for Login, Preferences, etc."""
 from models import *
 from errors import *
-from collections import OrderedDict
 
-class LoginManagerOld:
-    def check_user(self, email=None, password=None):
-        
-        user = User.query.filter_by(email=email, password=password).first()
-
-        if not user:
-            raise NoUserFoundError(email)
-
-        return True
-
+class LoginManager:
     def register_user(self, email=None, password=None):
         user = User.query.filter_by(email=email, password=password).all()
 
@@ -31,26 +21,18 @@ class LoginManagerOld:
 class UserManager:
     """Class that manages preferences, subscriptions, etc."""
     def get_all_websites(self):
-        all_data = OrderedDict()
-        categories = Category.query.all()
+        """Returns all websites in the system. These are used by the views to render all choices."""        
+        return Website.query.all()
 
-        # Produce an ordered dict with keys are cateogry names, so that these can
-        # rendered in exactly the same order in the view        
-        for cat in categories:
-            web = cat.websites.all()
-            if web:
-                if cat.name in all_data:
-                    all_data[cat.name].append(web)
-                else:
-                    all_data[cat.name] = web
-        
-        return all_data
+    def get_subscriptions(self, user):
+        return user.websites
 
-    def get_subscriptions(self, email):
-        return User.query.filter_by(email=email).first().sites
+    def update_subscriptions(self, user, websites):
+        if user.websites is None:
+            user.wesbites = list()
 
-    def update_subscriptions(self, email, sites):
-        user = User.query.filter_by(email=email).first()
-        user.sites = sites
+        for w in websites:
+            user.websites.append(w)
+
         db.session.add(user)
         db.session.commit()
